5dd1410047da3e0ace26d6c77a8dca317bda75b0
= <d.sek464@gmail.com>
2021-11-08 22:05:53 +0100

    added some explanations in the comments

--- block.js
+++ block.js
@@ -8,33 +8,35 @@ class Block {
         this.timestamp = timestamp; // time of creation of the block in unix seconds
         this.data = data; // data about the transactions happening in the block
         this.previousHash = previousHash; // index of the previous block
+        this.nonce = 0; 
         this.hash = this.calculateHash(); // sha256 hash of the block
-        this.nonce = 0; // number of tries to find a valid hash
     }
 
     calculateHash(){
         return SHA256(this.index + this.previousHash + this.timestamp
-             + JSON.stringify(this.data) + this.nonce).toString();
+             + JSON.stringify(this.data) + this.nonce).toString(); 
     }
-    
+        //Choosing leading zeroes for a “nonce” is just a convention. You could specify any sequence of leading digits,
+         //and the difficulty would be the same. Instead of “000” you could look for “123”. The difficulty lies in matching a specified sequence, 
+         //and each possible sequence is equally difficult to match. 
+        //The difficulty stems from having to match an exact sequence, not from the values that sequence has.
     proofOfWork = async (difficulty) => {
 
-    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
-        console.log("first condition: "+this.hash.substring(0, difficulty))
-        console.log("2nd condition: "+Array(difficulty + 1).join("0"))
-        // console.log("Block index: "+ this.index)
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){ // 
+        // console.log(this.hash.substring(0, difficulty))
         //await asyncFunction(1000)
         this.nonce++;
         this.hash = this.calculateHash();
-        // console.log("trying: ", this.hash);
+
+        //block no: ${this.index} 
+        console.log(`trying: ${this.hash} `);
 
     }
-    console.log("award:: "+this.hash.substring(0, difficulty))
-    console.log("award:: 2: "+Array(difficulty + 1).join("0"))
-    // console.log("Block mined: "+ this.index +"has hash: "+ this.hash);
+
+    console.log("Block mined: "+ this.hash);
 }
 
-} // end of class
+} // end of block class
 
 
 
@@ -44,22 +46,19 @@ class blockchain{
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2; 
     }
-    //create a genesis block method
+   
     createGenesisBlock(){
-        return new Block(0, Date.now().toString, "Genesis Block", "0");
+        return new Block(0, Date().toString, "Genesis Block", "0");
     }
-    //get last block method
     getLastBlock(){
         return this.chain[this.chain.length - 1];
     }
-    // add block method
     addBlock(newBlock){
         newBlock.previousHash = this.getLastBlock().hash;
         newBlock.hash = newBlock.calculateHash();
         newBlock.proofOfWork(this.difficulty);
         this.chain.push(newBlock);
     }
-    // check if chain is valid method
     isChainValid(){
         for(let i = 1; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
@@ -76,21 +75,39 @@ class blockchain{
 
 }
 test = new blockchain();
+var date = new Date(Date.now());
+function getDate(){
+    return date.getDate() 
+    + "/" + date.getMonth() 
+    + "/" + date.getFullYear() 
+    + " " + date.getHours() 
+    + ":" + date.getMinutes() 
+    + ":" + date.getSeconds();
+}
 
-test.addBlock(
-  new Block(1, Date.now().toString(), {
-    sender: "test1",
-    recipient: "test2",
-    quantity: 1
-  })
-);
-test.addBlock(
-  new Block(2, Date.now().toString(), {
-    sender: "test2",
-    recipient: "test1",
-    quantity: 15
-  })
-);
-console.log(JSON.stringify(test,null,4))
+console.log(test.chain)
+console.log("=====================================================================================================================================")
 
+console.log(test.addBlock(
+    new Block(1, getDate(), {
+      sender: "test1",
+      recipient: "test2",
+      quantity: 1
+       
+    })
+  ))
+
+  console.log(test.addBlock(
+    new Block(2, getDate()), { //16.11.2021 16:45:34
+      sender: "test2",
+      recipient: "test1",
+      quantity: 10
+    })
+  )
+
+
+console.log("=====================================================================================================================================")
+console.log(JSON.stringify(test, null, 4));
 // console.log(test)
+//
+
