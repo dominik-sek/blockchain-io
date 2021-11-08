526efffa188601b8eb2e8d19cffd955abc30450f
gothic459 <d.sek464@gmail.com>
2021-11-08 12:58:10 +0100

    async await show each step

--- block.js
+++ block.js
@@ -1,10 +1,12 @@
 //create a blockchain block class
 const SHA256 = require("crypto-js/sha256");
+const asyncFunction = (t) => new Promise(resolve => setTimeout(resolve, t));
 class Block {
+    
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index; // unique id to keep track of the block
         this.timestamp = timestamp; // time of creation of the block in unix seconds
-        this.data = data; // data ab the transactions happening in the block
+        this.data = data; // data about the transactions happening in the block
         this.previousHash = previousHash; // index of the previous block
         this.hash = this.calculateHash(); // sha256 hash of the block
         this.nonce = 0; // number of tries to find a valid hash
@@ -14,25 +16,33 @@ class Block {
         return SHA256(this.index + this.previousHash + this.timestamp
              + JSON.stringify(this.data) + this.nonce).toString();
     }
+    
+    proofOfWork = async (difficulty) => {
 
-    // proof of work algorithm
-proofOfWork = (difficulty) => {
     while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+        console.log("first condition: "+this.hash.substring(0, difficulty))
+        console.log("2nd condition: "+Array(difficulty + 1).join("0"))
+        // console.log("Block index: "+ this.index)
+        //await asyncFunction(1000)
         this.nonce++;
         this.hash = this.calculateHash();
-        console.log("%c checking: " + this.hash, 'color: red');
-        
+        // console.log("trying: ", this.hash);
+
     }
-    console.log("Block mined: " + this.hash, 'color: green');
+    console.log("award:: "+this.hash.substring(0, difficulty))
+    console.log("award:: 2: "+Array(difficulty + 1).join("0"))
+    // console.log("Block mined: "+ this.index +"has hash: "+ this.hash);
 }
-}
+
+} // end of class
+
 
 
 
 class blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 1;
+        this.difficulty = 2; 
     }
     //create a genesis block method
     createGenesisBlock(){
@@ -46,7 +56,7 @@ class blockchain{
     addBlock(newBlock){
         newBlock.previousHash = this.getLastBlock().hash;
         newBlock.hash = newBlock.calculateHash();
-        newBlock.proofOfWork(this.difficulty); 
+        newBlock.proofOfWork(this.difficulty);
         this.chain.push(newBlock);
     }
     // check if chain is valid method
@@ -81,6 +91,6 @@ test.addBlock(
     quantity: 15
   })
 );
+console.log(JSON.stringify(test,null,4))
 
-
-console.log(test)
+// console.log(test)
